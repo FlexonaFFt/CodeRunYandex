379229a0ad1b52346cce94407019e7b17f6a504d
FlexonaFFt <flexonafft@bk.ru>
2024-08-01 13:07:50 +0300

    --load day12 3

--- RunSeason2/backend/podstroka.py
+++ RunSeason2/backend/podstroka.py
@@ -1,3 +1,4 @@
+'''
 def find_most_common_pair(text):
     pair_freq = {}
 
@@ -15,3 +16,27 @@ def find_most_common_pair(text):
 if __name__ == '__main__':
     text = input()
     find_most_common_pair(text)
+'''
+
+# Это решение прошло все тесты
+
+def find_podstroka():
+    text = input()
+    words = text.split()
+    pair_counter = {}
+
+    for word in words:
+        for i in range(len(word) - 1):
+            pair = word[i:i + 2]
+            if pair in pair_counter:
+                pair_counter[pair] += 1
+            else:
+                pair_counter[pair] = 1
+
+    max_count = max(pair_counter.values())
+    max_pair = max(pair for pair, count in pair_counter.items()
+        if count == max_count)
+    print(max_pair)
+
+if __name__ == '__main__':
+    find_podstroka()
