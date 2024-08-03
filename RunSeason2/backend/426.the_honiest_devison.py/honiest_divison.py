7b8ca6ed2bbb110e6642d152e46b88e5fd53e2dd
FlexonaFFt <flexonafft@bk.ru>
2024-08-03 16:45:54 +0300

    --load day14 1

---
+++ RunSeason2/backend/426.the_honiest_devison.py/honiest_divison.py
@@ -0,0 +1,63 @@
+'''
+Решение ломается на 3ем тесте, а их 72+ :)
+
+from collections import Counter
+
+def find_the_honiest_divison(str):
+    char_count = Counter(str)
+    min_count = min(char_count.values())
+    print(min_count)
+
+
+if __name__ == '__main__':
+    string = str(input())
+    find_the_honiest_divison(string)
+'''
+
+'''
+Это решение также ломается на 3ем тесте
+
+def can_divive_string(str):
+    char_count = {}
+    for char in str:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+
+    max_guests = 1
+    length = len(str)
+
+    for k in range(1, length + 1):
+        if length % k == 0:
+            if all(count % k == 0 for count in char_count.values()):
+                max_guests = k
+
+    return max_guests
+
+if __name__ == '__main__':
+    string = str(input())
+    print(can_divive_string(string))
+'''
+
+def can_divive_string(str):
+    char_count = {}
+    for char in str:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+
+    max_guests = 1
+    length = len(str)
+
+    for k in range(1, length + 1):
+        if length % k == 0:
+            if all(count % k == 0 for count in char_count.values()):
+                max_guests = k
+
+    return max_guests
+
+if __name__ == '__main__':
+    string = str(input())
+    print(can_divive_string(string))
